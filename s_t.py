04624ecab39c4a55c88dbf50ef1f78bc3b9e9545
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-10-02 21:42:58 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -1,38 +1,54 @@
 import os
 import streamlit as st
 from bokeh.models.widgets import Button
-#from bokeh.io import show
-#from bokeh.models import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 from PIL import Image
 import time
 import glob
-
-
-
 from gtts import gTTS
 from googletrans import Translator
 
+# Custom CSS for fonts and centering image
+st.markdown("""
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Lexend:wght@600&display=swap');
+    .title-font {
+        font-family: 'Lexend', sans-serif;
+        font-size: 36px;
+        text-align: center;
+    }
+    .paragraph-font {
+        font-family: 'Inter', sans-serif;
+        font-size: 18px;
+        text-align: justify;
+    }
+    .center-img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
-st.title("TRADUCTOR.")
-st.subheader("Escucho lo que quieres traducir.")
+# Title with custom font
+st.markdown('<p class="title-font">TRADUCTOR</p>', unsafe_allow_html=True)
+st.markdown('<p class="paragraph-font">Escucho lo que quieres traducir.</p>', unsafe_allow_html=True)
 
-
+# Display centered image
 image = Image.open('OIG7.jpg')
+st.image(image, width=300, caption="Traductor", use_column_width='auto')
 
-st.image(image,width=300)
 with st.sidebar:
-    st.subheader("Traductor.")
+    st.subheader("Traductor")
     st.write("Presiona el botón, cuando escuches la señal "
-                 "habla lo que quieres traducir, luego selecciona"   
-                 " la configuración de lenguaje que necesites.")
+             "habla lo que quieres traducir, luego selecciona"
+             " la configuración de lenguaje que necesites.")
 
+st.markdown('<p class="paragraph-font">Toca el botón y habla lo que quieres traducir.</p>', unsafe_allow_html=True)
 
-st.write("Toca el Botón y habla lo que quires traducir")
-
-stt_button = Button(label=" Escuchar  🎤", width=300,  height=50)
-
+# Button for speech-to-text
+stt_button = Button(label="Escuchar 🎤", width=300, height=50)
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
@@ -45,140 +61,110 @@ stt_button.js_on_event("button_click", CustomJS(code="""
                 value += e.results[i][0].transcript;
             }
         }
-        if ( value != "") {
+        if (value != "") {
             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
         }
     }
     recognition.start();
     """))
 
+# Streamlit Bokeh events for handling speech-to-text
 result = streamlit_bokeh_events(
     stt_button,
     events="GET_TEXT",
     key="listen",
     refresh_on_update=False,
     override_height=75,
-    debounce_time=0)
+    debounce_time=0
+)
 
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
-    try:
+
+    # Create temp folder if not exists
+    if not os.path.exists("temp"):
         os.mkdir("temp")
-    except:
-        pass
-    st.title("Texto a Audio")
+
+    st.markdown('<p class="title-font">Texto a Audio</p>', unsafe_allow_html=True)
     translator = Translator()
-    
+
+    # Get spoken text and choose input/output languages
     text = str(result.get("GET_TEXT"))
     in_lang = st.selectbox(
         "Selecciona el lenguaje de Entrada",
-        ("Inglés", "Español", "Alemán", "Italiano", "Francés", "Japonés"),
+        ("Inglés", "Español", "Alemán", "Italiano", "Francés", "Japonés")
     )
-    if in_lang == "Inglés":
-        input_language = "en"
-    elif in_lang == "Español":
-        input_language = "es"
-    elif in_lang == "Alemán":
-        input_language = "de"
-    elif in_lang == "Italiano":
-        input_language = "ita"
-    elif in_lang == "Francés":
-        input_language = "fr"
-    elif in_lang == "Japonés":
-        input_language = "ja"
-    
+
+    input_language = {
+        "Inglés": "en",
+        "Español": "es",
+        "Alemán": "de",
+        "Italiano": "it",
+        "Francés": "fr",
+        "Japonés": "ja"
+    }[in_lang]
+
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Español", "Alemán", "Coreano", "Mandarín", "Japonés")
     )
-    if out_lang == "Inglés":
-        output_language = "en"
-    elif out_lang == "Español":
-        output_language = "es"
-    elif out_lang == "Alemán":
-        output_language = "de"
-    elif out_lang == "Italiano":
-        output_language = "it-ita"
-    elif out_lang == "Mandarín":
-        output_language = "zh-cn"
-    elif out_lang == "Japonés":
-        output_language = "ja"
-    
+
+    output_language = {
+        "Inglés": "en",
+        "Español": "es",
+        "Alemán": "de",
+        "Coreano": "ko",
+        "Mandarín": "zh-cn",
+        "Japonés": "ja"
+    }[out_lang]
+
+    # Select English accent if applicable
     english_accent = st.selectbox(
         "Selecciona el acento",
-        (
-            "Defecto",
-            "Español",
-            "Reino Unido",
-            "Estados Unidos",
-            "Canada",
-            "Australia",
-            "Irlanda",
-            "Sudáfrica",
-        ),
+        ("Defecto", "Español", "Reino Unido", "Estados Unidos", "Canada", "Australia", "Irlanda", "Sudáfrica")
     )
-    
-    if english_accent == "Defecto":
-        tld = "com"
-    elif english_accent == "Español":
-        tld = "com.mx"
-    
-    elif english_accent == "Reino Unido":
-        tld = "co.uk"
-    elif english_accent == "Estados Unidos":
-        tld = "com"
-    elif english_accent == "Canada":
-        tld = "ca"
-    elif english_accent == "Australia":
-        tld = "com.au"
-    elif english_accent == "Irlanda":
-        tld = "ie"
-    elif english_accent == "Sudáfrica":
-        tld = "co.za"
-    
-    
+
+    tld = {
+        "Defecto": "com",
+        "Español": "com.mx",
+        "Reino Unido": "co.uk",
+        "Estados Unidos": "com",
+        "Canada": "ca",
+        "Australia": "com.au",
+        "Irlanda": "ie",
+        "Sudáfrica": "co.za"
+    }[english_accent]
+
+    # Function to translate and convert text to speech
     def text_to_speech(input_language, output_language, text, tld):
         translation = translator.translate(text, src=input_language, dest=output_language)
         trans_text = translation.text
         tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
-        try:
-            my_file_name = text[0:20]
-        except:
-            my_file_name = "audio"
+        my_file_name = text[:20] if len(text) > 20 else "audio"
         tts.save(f"temp/{my_file_name}.mp3")
         return my_file_name, trans_text
-    
-    
+
     display_output_text = st.checkbox("Mostrar el texto")
-    
-    if st.button("convertir"):
+
+    if st.button("Convertir"):
         result, output_text = text_to_speech(input_language, output_language, text, tld)
         audio_file = open(f"temp/{result}.mp3", "rb")
         audio_bytes = audio_file.read()
-        st.markdown(f"## Tú audio:")
+        st.markdown(f"## Tu audio:")
         st.audio(audio_bytes, format="audio/mp3", start_time=0)
-    
+
         if display_output_text:
             st.markdown(f"## Texto de salida:")
-            st.write(f" {output_text}")
-    
-    
+            st.write(output_text)
+
+    # Function to remove old files
     def remove_files(n):
-        mp3_files = glob.glob("temp/*mp3")
-        if len(mp3_files) != 0:
-            now = time.time()
-            n_days = n * 86400
-            for f in mp3_files:
-                if os.stat(f).st_mtime < now - n_days:
-                    os.remove(f)
-                    print("Deleted ", f)
+        mp3_files = glob.glob("temp/*.mp3")
+        now = time.time()
+        n_days = n * 86400
+        for f in mp3_files:
+            if os.stat(f).st_mtime < now - n_days:
+                os.remove(f)
 
     remove_files(7)
-           
-
-
-        
-    
-
-
